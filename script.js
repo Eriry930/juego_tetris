e9151c929dcbaffd002cd4df2f1186f4bf133730
Eriry930 <erika-915@hotmail.com>
2021-12-10 11:54:53 -0500

    numero 1 creacion archivo css, modificacion de html y js

---
+++ script.js
@@ -0,0 +1,18 @@
+const canvas =document.getElementById("tetris"); //llama al canvas en el JavaScript
+const context = canvas.getContext ("2d"); // el contexto será en 2 dimenciones
+
+context.scale(20,20); //la escala del canvas para ver el juego 20 columnas y 25 filas dependiendo del alto y ancho del canvas
+context.fillStyle= #000 ; // dibujar de color negro pero no funciona
+context.fillRect (0 , 0 , canvas.width , canvas.height);
+
+funtion update (time=0){
+    const deltaTime = time - lastTime;
+    lasTime time=
+
+}
+
+
+
+
+
+
