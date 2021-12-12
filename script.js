2a0b5574f84cad24d91601bcb56a34650e1af359
Eriry930 <erika-915@hotmail.com>
2021-12-11 19:53:10 -0500

    numero 4 adelanto javascript

--- script.js
+++ script.js
@@ -1,107 +1,214 @@
-const canvas = document.getElementById("tetris"); //llama al canvas en el JavaScript
-const context = canvas.getContext ("2d") // el contexto será en 2 dimenciones
+const canvas = document.getElementById("tetris");
+const ctx = canvas.getContext('2d');
 
-context.fillRect(0,0,1,1)
-const scale = 20
-context.scale(scale, scale); //la escala del canvas para ver el juego 20 columnas y 25 filas dependiendo del alto y ancho del canvas
+const scale = 20;
 
-const tWidth = canvas.width / scale
-const tHeight = canvas.height / scale
+ctx.scale(scale, scale);
+
+const tWidth = canvas.width / scale;
+const tHeight = canvas.height / scale;
 
 const pieces = [
     [
-        [1,1],
-        [1,1],
+        [1, 1],
+        [1, 1]
     ],
     [
-        [0,1,0,0],
-        [0,1,0,0],
-        [0,1,0,0],
-        [0,1,0,0],
+        [0, 2, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 0, 0]
     ],
     [
-        [0,0,0],
-        [0,1,1],
-        [1,1,0],
+        [0, 0, 0],
+        [3, 3, 0],
+        [0, 3, 3]
     ],
     [
-        [0,0,1],
-        [0,0,1],
-        [0,1,1],
+        [0, 0, 0],
+        [0, 4, 4],
+        [4, 4, 0]
     ],
     [
-        [0,0,0],
-        [1,1,1],
-        [0,1,0],
+        [5, 0, 0],
+        [5, 0, 0],
+        [5, 5, 0]
+    ],
+    [
+        [0, 0, 6],
+        [0, 0, 6],
+        [0, 6, 6]
+    ],
+    [
+        [0, 0, 0],
+        [7, 7, 7],
+        [0, 7, 0]
     ]
-]
-const arena = []
+];
+const colors = [
+    null,
+    '#FF0D72',
+    '#0DC2FF',
+    '#0DFF72',
+    '#F538FF',
+    '#FF8E0D',
+    '#FFE138',
+    '#3877FF'
+];
+
+let arena = [];
+
+let rand;
 
 const player = {
-    pos: {x:0, y:0},
-    matrix: pieces [3]
+    pos: {x: 0, y: 1},
+    matrix: null,
+    color: null
 }
 
+rand = Math.floor(Math.random() * pieces.length);
+player.matrix = pieces[rand];
+player.color = colors[rand+1];
+
 function drawMatrix(matrix, x, y) {
-    for (let i = 0; i < matrix.length; i++){
-        for (let j = 0; j < matrix[i].length; j++){
-            if (matrix [i] [j])
-                context.fillRect(x + j, y + i, 1, 1)
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j])
+                ctx.fillRect(x + j, y + i, 1, 1);
+        }
+    }
+}
+
+function rotateMatrix(matrix, dir) {
+    let newMatrix = [];
+
+    for (let i in matrix)
+        newMatrix.push([]);
+
+    if (dir === 1) {
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                newMatrix[j][matrix.length - i - 1] = matrix[i][j];
+            }
+        }
+    } else {
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                newMatrix[matrix.length - j - 1][i] = matrix[i][j];
+            }
+        }
+    }
+
+    return newMatrix;
+}
+
+function collides(player, arena) {
+    for (let i = 0; i < player.matrix.length; i++) {
+        for (let j = 0; j < player.matrix[i].length; j++) {
+            if (player.matrix[i][j] && arena[player.pos.y + i + 1][player.pos.x + j + 1])
+                return 1;
+        }
+    }
+
+    return 0;
+}
+
+
+
+
+
+function drawArena() {
+    for (let i = 1; i < arena.length-2; i++) {
+        for (let j = 1; j < arena[i].length-1; j++) {
+            if (arena[i][j]) {
+                ctx.fillStyle = colors[arena[i][j]];
+                ctx.fillRect(j-1, i-1, 1, 1);
+            }
         }
     }
 }
 
 function initArena() {
-    const r = new Array(tWidth + 2).fill(1)
-    arena.push(r)
+    arena = [];
 
-    for (let i = 0; i < tHeight; i++){
-        let row = new Array(tWidth).fill(0)
-        row.push(1)
-        row.unshift(1)
+    const r = new Array(tWidth + 2).fill(1);
+    arena.push(r);
 
-        arena.push(row)
+    for (let i = 0; i < tHeight; i++) {
+        let row = new Array(tWidth).fill(0);
+        row.push(1);
+        row.unshift(1);
+
+        arena.push(row);
     }
-        arena.push(r)
+
+    arena.push(r);
+    arena.push(r);
 }
 
 
-let interval = 1000
-let lastTime = 0
-let count= 0
+let interval = 1000;
+let lastTime = 0;
+let count = 0;
 
-function update(time =0) {
-    const dt = time - lastTime
-    lastTime = time
-    count += dt
+function update(time = 0) {
 
-    if (count >= interval){
-        player.pos.y++
-        count = 0
+    const dt = time - lastTime;
+    lastTime = time;
+    count += dt;
+
+    if (count >= interval) {
+        player.pos.y++;
+        count = 0;
     }
 
-    context.fillStyle = "#000000"
-    context.fillRect(0, 0, canvas.clientWidth, canvas.height)
+    if (collides(player, arena)) {
+        mergeArena(player.matrix, player.pos.x, player.pos.y-1);
+        clearBlocks();
+        gameOver();
 
-    context.fillStyle = "red"
-    drawMatrix(player.matrix, player.pos.x, player.pos.y)
+        player.pos.y = 1;
+        player.pos.x = 0;
 
-    requestAnimationFrame(update)
+        rand = Math.floor(Math.random() * pieces.length);
+        player.matrix = pieces[rand];
+        player.color = colors[rand+1];
+
+        interval = 1000;
+    }
+
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    drawArena();
+    ctx.fillStyle = player.color;
+    drawMatrix(player.matrix, player.pos.x, player.pos.y);
+
+    requestAnimationFrame(update);
 }
 
-document.addEventListener('keydown', function(tecla){
+document.addEventListener("keydown", event => {
 
-    if(tecla.key == "ArrowLeft"){
-        player.pos.x--
+    if (event.keyCode === 37 && interval-1) {
+        player.pos.x--;
+        if (collides(player, arena))
+            player.pos.x++;
+    } else if (event.keyCode === 39 && interval-1) {
+        player.pos.x++;
+        if (collides(player, arena))
+            player.pos.x--;
+    } else if (event.keyCode === 40) {
+        player.pos.y++;
+        count = 0;
+    } else if (event.keyCode === 38) {
+        player.matrix = rotateMatrix(player.matrix, 1);
+        if (collides(player, arena))
+            player.matrix = rotateMatrix(player.matrix, -1);
+    } else if (event.keyCode === 32) {
+        interval = 1;
     }
-    if(tecla.key == "ArrowRight"){
-        player.pos.x++
-    }
-    if(tecla.key == "ArrowDown"){
-        player.pos.y++
-        count = 0
-    }
-})
 
-initArena()
-update()
+});
+
+initArena();
+update();
